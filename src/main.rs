16afb8cf7a46557c131b4e0d5822c38bbcf0b950
yusuke ota <33338085+yusuke-ota@users.noreply.github.com>
2020-04-23 11:53:51 +0900

    html表示成功

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use tide;
+use async_std::fs::File;
+use async_std::io::BufReader;
+
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
     let mut app = tide::new();
-    app.at("/").get(|_| async move { "Hello, world!" });
+    app.at("/").get(|_| async move {"hello"});
+    app.at("/hello").get( |_| async move {
+        let file = File::open("hello.html").await.unwrap();
+        let reader = BufReader::new(file);
+        tide::Response::new(200)
+            .body(reader)
+            .set_mime(mime::TEXT_HTML)
+    });
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
